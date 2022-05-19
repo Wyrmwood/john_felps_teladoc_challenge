ff09685c68fe317a87a1ba7e54c81bfd44b48c9a
Wyrmwood <john.screwtape@gmail.com>
2022-05-19 07:59:10 -0600

    typo, docstring summary

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ URL = "https://jsonmock.hackerrank.com/api/movies/search/"
 
 def query_title(query: str) -> list[str]:
     """
+    Query a title at the endpoint, parse the total_pages and retrieve all titles matching the query.
 
     Args:
-        query: A string of substring of a movie title (case-insensitive)
+        query: A string or substring of a movie title (case-insensitive)
 
     Returns:
         A sorted list of matching titles in the mock database
