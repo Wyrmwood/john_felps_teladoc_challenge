7e41da052248d0e07d56fabd18793165ff95c7e1
Wyrmwood <john.screwtape@gmail.com>
2022-05-19 07:37:26 -0600

    add docstring and a few other tests

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ URL = "https://jsonmock.hackerrank.com/api/movies/search/"
 
 
 def query_title(query: str) -> list[str]:
+    """
+
+    Args:
+        query: A string of substring of a movie title (case-insensitive)
+
+    Returns:
+        A sorted list of matching titles in the mock database
+    """
     retrieved_titles = []
     response = requests.get(URL, params={"Title": query})
     data = response.json()
@@ -22,6 +30,7 @@ def query_title(query: str) -> list[str]:
 
 
 if __name__ == "__main__":
-    TITLES = query_title("spiderman")
-    print(yaml.safe_dump(TITLES))
-    print(len(TITLES))
+    for NAME in "batman", "superman", "spiderman":
+        TITLES = query_title(NAME)
+        print(yaml.safe_dump(TITLES))
+        print(len(TITLES))
